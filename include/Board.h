1554aa56d32146fb16543632278c01560b26b522
alesharay <alesha.ray@g.austincc.edu>
2017-09-21 11:14:52 -0500

    added stub board class

---
+++ include/Board.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<iostream>
+#include"catch.hpp"
+using namespace std;
+
+class Board
+{
+private:
+	// private data members
+
+	// private functions
+
+public:
+	Board();
+};
